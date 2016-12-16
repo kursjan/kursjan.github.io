d94bfebf405b42ad1991edf0d263495ebcffe77c
Jan Kurs <kurs.jan@gmail.com>
2016-12-16 08:12:20 +0100

    matching tags moved to appendix

--- petitparser2/pillar-book/loadPetitParser2.sh
+++ petitparser2/pillar-book/loadPetitParser2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+repo=$1
+
 ./pharo "Pharo.image" save "PharoPP2"
 
 ./pharo "PharoPP2.image" eval --save "
 | repo |
-repo := './..' asFileReference.
+repo := '${repo}' asFileReference.
 repo := repo absolutePath segments inject: 'filetree://' into: [ :path :seg | path, '/', seg ].
 Metacello new
 	baseline: 'PetitParser2';
